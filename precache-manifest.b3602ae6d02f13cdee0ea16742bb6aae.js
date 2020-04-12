d46533558bf09e531960b312a805835fdec41b45
ArtemIgnatiev908976 <ignatev.artem.nik@gmail.com>
2020-04-13 00:02:41 +0400

    Updates

---
+++ precache-manifest.b3602ae6d02f13cdee0ea16742bb6aae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93916dd53d7204d1bfed29cd17ac0497",
+    "url": "/cryptocurrency_converter_ReactJS_MobX_Material-UI/index.html"
+  },
+  {
+    "revision": "2a7f43c1ae03db9569ad",
+    "url": "/cryptocurrency_converter_ReactJS_MobX_Material-UI/static/css/main.b2283f6b.chunk.css"
+  },
+  {
+    "revision": "a9594d9cee462dd12e6c",
+    "url": "/cryptocurrency_converter_ReactJS_MobX_Material-UI/static/js/2.36ac1a20.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/cryptocurrency_converter_ReactJS_MobX_Material-UI/static/js/2.36ac1a20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a7f43c1ae03db9569ad",
+    "url": "/cryptocurrency_converter_ReactJS_MobX_Material-UI/static/js/main.f920a612.chunk.js"
+  },
+  {
+    "revision": "edf45c3a4f1b52f22ba1",
+    "url": "/cryptocurrency_converter_ReactJS_MobX_Material-UI/static/js/runtime-main.b0dbcc1c.js"
+  }
+]);
